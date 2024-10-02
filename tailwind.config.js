116827bffc003d7d85730c5a9b41d0d20d3b3fb9
cethien <borislaw.sotnikow@gmx.de>
2024-10-02 16:58:52 +0200

    feat: styling, text, functionality
    - placeholder text
    - meta data
    - dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 import { addDynamicIconSelectors } from '@iconify/tailwind'
+import typography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
 	theme: {
 		extend: {}
 	},
-	plugins: [addDynamicIconSelectors()]
+	plugins: [addDynamicIconSelectors(), typography()]
 }
