ef8395f1589e67ada75566baa54cca463e92e9d6
cethien <borislaw.sotnikow@gmx.de>
2024-12-15 18:39:05 +0100

    chore: conditional devShell install hook

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in 
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        bun
+      ];
+
+      shellHook = ''
+        if [ ! -f .envrc ]; then
+          echo "use flake" >> .envrc && direnv allow          
+        fi
+        
+        if [ ! -d node_modules ]; then
+          bun install
+        fi
+      '';
+    };
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+}
